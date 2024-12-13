a0c707b4b4ea48aa0cdd61e884d5d03e5177d1c8
Ivan Bertona <ivan.bertona@gmail.com>
2024-12-13 17:37:37 +0100

    Add env utils to shellz package.

--- consolez/utils_test.go
+++ consolez/utils_test.go
@@ -7,22 +7,22 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-type Suite struct {
+type UtilsSuite struct {
 	// intentionally empty
 }
 
-func TestSuite(t *testing.T) {
-	fixturez.RunSuite(t, &Suite{})
+func TestUtilsSuite(t *testing.T) {
+	fixturez.RunSuite(t, &UtilsSuite{})
 }
 
-func (*Suite) TestAlignRight(g *WithT) {
+func (*UtilsSuite) TestAlignRight(g *WithT) {
 	g.Expect(alignRight("abcd", 4)).To(Equal("abcd"))
 	g.Expect(alignRight("cd", 2)).To(Equal("..cd"))
 	g.Expect(alignRight("abcd", 6)).To(Equal("..abcd"))
 	g.Expect(alignRight("abcdef", 4)).To(Equal("...f"))
 }
 
-func (*Suite) TestTruncateLeft(g *WithT) {
+func (*UtilsSuite) TestTruncateLeft(g *WithT) {
 	g.Expect(truncateLeft("abcd", 4)).To(Equal("abcd"))
 	g.Expect(truncateLeft("cd", 2)).To(Equal("cd"))
 	g.Expect(truncateLeft("abcd", 6)).To(Equal("abcd"))
